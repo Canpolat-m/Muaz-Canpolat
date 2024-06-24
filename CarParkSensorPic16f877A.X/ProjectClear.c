74108c84b0a80654202e908d6969369a99bce532
Canpolat-m <muaz.canpolat@gmail.com>
2024-06-24 12:17:54 +0300

    ProjectClear.c

--- CarParkSensorPic16f877A.X/ProjectClear.c
+++ CarParkSensorPic16f877A.X/ProjectClear.c
@@ -1,9 +1,7 @@
-#include <xc.h>                // include device-specific header
-#include <pic16f877a.h>        // PIC16F877A specific definitions
-#include <string.h>            // string library
-#define _XTAL_FREQ 8000000    // define the oscillator frequency (20 MHz)
-
-// CONFIG
+#include <xc.h>               
+#include <pic16f877a.h>        
+           
+#define _XTAL_FREQ 8000000    // define the oscillator frequency 8 MHz
 #pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
 #pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
 #pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
@@ -13,15 +11,13 @@
 #pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
 #pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
 
-#define RS PORTCbits.RC4        // Define RS pin for LCD
-//#define RW PORTDbits.RD0        // Define RW pin for LCD
-#define EN PORTCbits.RC5        // Define EN pin for LCD
+#define RS PORTCbits.RC4              
+#define EN PORTCbits.RC5        
 
-// Function to send commands to the LCD
+
 void lcd_cmd(unsigned char cmd) {
     PORTD = (cmd & 0xF0);       // Send higher nibble
     EN = 1;                     // Enable high
-    //RW = 0;                     // Write operation
     RS = 0;                     // Command mode
     __delay_ms(10);             // Delay
     EN = 0;                     // Enable low
@@ -37,30 +33,30 @@ void lcd_cmd(unsigned char cmd) {
 void lcd_data(unsigned char data) {
     PORTD = (data & 0xF0);      // Send higher nibble
     EN = 1;                     // Enable high
-    //RW = 0;                     // Write operation
     RS = 1;                     // Data mode
     __delay_ms(10);             // Delay
-    EN = 0;                     // Enable low
-    PORTD = ((data << 4) & 0xF0);// Send lower nibble
-    EN = 1;                     // Enable high
-    //RW = 0;                     // Write operation
-    RS = 1;                     // Data mode
-    __delay_ms(10);             // Delay
-    EN = 0;                     // Enable low
+    EN = 0;                     
+    PORTD = ((data << 4) & 0xF0);
+    EN = 1;                     
+    RS = 1;                     
+    __delay_ms(10);             
+    EN = 0;                     
 }
 
 // Function to initialize the LCD
 void lcd_init() {
+    
     lcd_cmd(0x02);              // Initialize LCD in 4-bit mode
     lcd_cmd(0x28);              // 4-bit mode, 2 lines, 5x7 matrix
     lcd_cmd(0x0C);              // Display on, cursor off
     lcd_cmd(0x06);              // Increment cursor
     lcd_cmd(0x01);              // Clear display
     __delay_ms(20);             // Delay
+    
 }
 
 // Function to send a string to the LCD
-void lcd_string(const unsigned char *str, unsigned char num) {
+void Lcd_string(const unsigned char *str, unsigned char num) {
     unsigned char i;
     for(i = 0; i < num; i++) {
         lcd_data(str[i]);       // Send each character to the LCD
@@ -84,7 +80,7 @@ void main() {
     
     lcd_cmd(0x85);  // Set cursor position
         __delay_ms(10);
-        lcd_string("CAUTION", 7); // Display "L" for left sensor
+        Lcd_string("CAUTION", 7); // Display "L" for left sensor
         __delay_ms(10); 
     while(1) {                  // Infinite loop
         TMR1H = 0;                //Sets the Initial Value of Timer
@@ -108,7 +104,7 @@ void main() {
             __delay_ms(10);
             lcd_cmd(0xC2);
             __delay_ms(30);
-            lcd_string("   WARNING  ", 12); // Display "L" for left sensor
+            Lcd_string("   WARNING  ", 12);
             __delay_ms(100); }
         
         else if(distance <= 9 && distance > 2){
@@ -118,13 +114,13 @@ void main() {
             __delay_ms(10);
             lcd_cmd(0xC2);
             __delay_ms(100);
-            lcd_string("  !!HIT!!    ", 12); // Display "L" for left sensor
+            Lcd_string("  !!HIT!!    ", 12); 
             __delay_ms(60); }
         else{
             PORTCbits.RC3 = 0; 
             lcd_cmd(0xC2);
             __delay_ms(30);
-            lcd_string("Out of range", 12); // Display "L" for left sensor
+            Lcd_string("Out of range", 12);
             __delay_ms(100); 
         }
             
